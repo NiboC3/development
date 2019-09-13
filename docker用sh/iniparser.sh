5d7c4fd1ea1e4202de6b8d58f8664afe8ca1bb67
da-machi <machi_kat@yahoo.co.jp>
2019-09-14 00:48:25 +0900

    更新

--- docker用sh/iniparser.sh
+++ docker用sh/iniparser.sh
@@ -1,34 +1,58 @@
 #!/bin/bash
 
 #===========================================================================
-# Contents   : 部品
-#              iniファイルの変数取り込み用
+# Contents   : Parts
+#              ini(File) reader
+# Arguments  :1)inifile full path
 # Author     : da-machi
 # LastUpdate : 2019/09/08
 # Since      : 2019/09/08
-# Problem    : 変数重複を考慮していない
+# Problem    : 重複変数は後出し？上書きされるはず
 #===========================================================================
 
 #ファイル名(ini)
-v_ini_fn=$1/lintset.ini
-echo ${v_ini_fn}
-#ファイルを変数に取り込む
-var="$(<${v_ini_fn})"
+v_ini_fn=$1
 
-#[から始まる行を削除
-#\rを削除
-#\nを削除
-#コメント行を削除
-var=`echo "${var}" \
-| sed '/^\\[/d' \
-| sed '/^\r/d' \
-| sed '/^\n/d' \
-| sed '/^;/d' \
-`
+#ループしながらファイル内を1行ずつ処理する。
+while read line
+do
 
-#「:」を「=」に置き換え
-var=`echo "${var}" | sed -e 's/:/=/g'`
+  #空行か判定(結果出力は捨てる /dev/null)
+  echo "${line}" | grep "^\s*$" > /dev/null
+  ret0=$?;
 
-echo "${var}"
+  #コメントか判定
+  echo "${line}" | grep "^;" > /dev/null
+  ret1=$?
 
+  #セクションか確認
+  echo "${line}" | grep \\[.*\\] > /dev/null
+  ret2=$?
+
+  #空行 コメントの場合は読み飛ばす
+  if [ ${ret0} -eq 0 -o ${ret1} -eq 0 ]; then
+    #処理なし
+    :
+
+  #セクションの場合内容を退避する
+  elif [ ${ret2} -eq 0 ]; then
+    #セクション内容を退避
+    section=`echo ${line} | cut -b 2-$((${#line}-2))`
+
+  #それ以外の行(XXX:YYYみたいな行のはず)
+  else
+    #セクションが空でないか確認
+    test -z ${section};
+    ret4=$?;
+    #セクションが空ではない
+    #1行目がセクションから始まってないときの読み飛ばし
+    if [ ${ret4} -ne 0 ]; then
+      #文字列結合と文字置換( : -> = )「セクション_構成要素=内容」
+      var=`echo ${section}_${line} | sed -e 's/:/=/g'`
+      #標準出力
+      echo ${var}
+    fi
+  fi
+
+done < ${v_ini_fn}
 
